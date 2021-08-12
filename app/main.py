f60dde442a24b141da212a07d1c20eec093b7a51
Théa Grondier <thea@exoscale.ch>
2021-08-12 17:00:08 +0200

    Move website to kubernetes

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @app.route("/cv/")
 def send_cv():
-    return app.send_static_file('files/cv.pdf')
+    return app.send_static_file("files/cv.pdf")
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
